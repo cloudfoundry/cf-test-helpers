794f7890cbc8726bc96454b36e557701d06821ed
Karl Isenberg <kisenberg@pivotallabs.com>
2014-05-05 17:57:53 -0700

    Changed package to match new cloudfoundry-incubator location.
    Signed-off-by: Michael Fraenkel <fraenkel@us.ibm.com>

--- runner/run_test.go
+++ runner/run_test.go
@@ -1,11 +1,11 @@
 package runner_test
 
 import (
+	"github.com/cloudfoundry-incubator/cf-test-helpers/runner"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	. "github.com/onsi/gomega/gbytes"
 	. "github.com/onsi/gomega/gexec"
-	"github.com/pivotal-cf-experimental/cf-test-helpers/runner"
 )
 
 var _ = Describe("Run", func() {
