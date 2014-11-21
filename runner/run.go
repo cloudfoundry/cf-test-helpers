97f5e5f51dc1b3ba9a67ff4ce224cf9632865c0a
Amit Gupta <agupta@pivotal.io>
2014-11-21 11:47:38 -0800

    Add time information around commands
    Signed-off-by: Ted Young <tyoung@pivotal.io>

--- runner/run.go
+++ runner/run.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os/exec"
 	"strings"
+	"time"
 
 	"github.com/onsi/ginkgo"
 	"github.com/onsi/ginkgo/config"
@@ -12,6 +13,8 @@ import (
 	"github.com/onsi/gomega/gexec"
 )
 
+const timeFormat = "2006-01-02 15:04:00 (MST)"
+
 var CommandInterceptor = func(cmd *exec.Cmd) *exec.Cmd {
 	return cmd
 }
@@ -19,10 +22,13 @@ var CommandInterceptor = func(cmd *exec.Cmd) *exec.Cmd {
 func Run(executable string, args ...string) *gexec.Session {
 	cmd := exec.Command(executable, args...)
 
-	sayCommandWillRun(cmd)
+	startTime := time.Now()
+	sayCommandWillRun(startTime, cmd)
 
 	sess, err := gexec.Start(CommandInterceptor(cmd), ginkgo.GinkgoWriter, ginkgo.GinkgoWriter)
 	Expect(err).NotTo(HaveOccurred())
+
+	sayCommandDidRun(startTime, time.Now(), cmd)
 
 	return sess
 }
@@ -32,12 +38,23 @@ func Curl(args ...string) *gexec.Session {
 	return Run("curl", args...)
 }
 
-func sayCommandWillRun(cmd *exec.Cmd) {
+func sayCommandWillRun(startTime time.Time, cmd *exec.Cmd) {
 	startColor := ""
 	endColor := ""
 	if !config.DefaultReporterConfig.NoColor {
 		startColor = "\x1b[32m"
 		endColor = "\x1b[0m"
 	}
-	fmt.Fprintf(ginkgo.GinkgoWriter, "\n%s> %s %s\n", startColor, strings.Join(cmd.Args, " "), endColor)
+	fmt.Fprintf(ginkgo.GinkgoWriter, "\n%s[%s]> %s %s\n", startColor, startTime.UTC().Format(timeFormat), strings.Join(cmd.Args, " "), endColor)
+}
+
+func sayCommandDidRun(startTime, finishedTime time.Time, cmd *exec.Cmd) {
+	startColor := ""
+	endColor := ""
+	if !config.DefaultReporterConfig.NoColor {
+		startColor = "\x1b[33m"
+		endColor = "\x1b[0m"
+	}
+	elapsedTime := finishedTime.Sub(startTime)
+	fmt.Fprintf(ginkgo.GinkgoWriter, "\n%s[Completed: %s]> %s %s\n", startColor, elapsedTime, strings.Join(cmd.Args, " "), endColor)
 }
